4b3a975f71915d37a9a34db3f9f2b39ae9bedffb
Vivek Dattatreya <vivek.dattatreya@gmail.com>
2020-01-05 17:09:21 +0530

    This is My First Commit

---
+++ RabbitMq- MicroService/Publisher/ConsoleApp1/Consumer/Sender.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicQueue", false, false, false, null);
+
+                string message = "Getting Started with RabbitMQ Core Project";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicQueue", null, body);
+                Console.WriteLine("sent Following Message :" + message);
+
+
+            }
+
+            Console.WriteLine("Press [Enter] to exit the Sender App");
+            Console.ReadLine();
+        }
+    }
+}
